9a66267d3e858445c43b57cfd2ed60b0d69901c6
Andrei <andrei.buhus@yahoo.com>
2020-03-19 01:16:19 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  //pages
+  import Home from "./pages/Home.svelte";
+  import About from "./pages/About.svelte";
+  import Checkout from "./pages/Checkout.svelte";
+  import Login from "./pages/Login.svelte";
+  import Products from "./pages/Products.svelte";
+  import ProductTemplate from "./pages/ProductTemplate.svelte";
+
+  //router
+  import { Router, Route, Link } from "svelte-routing";
+</script>
+
+<style>
+
+</style>
+
+<Router>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/products">Products</Link>
+  </nav>
+  <Route path="/" component={Home} />
+  <Route path="/about" component={About} />
+  <Route path="/login" component={Login} />
+  <Route path="/products" component={Products} />
+  <Route path="/products/:id" component={ProductTemplate} />
+</Router>
---
+++ src/pages/ProductTemplate.svelte
@@ -0,0 +1,1 @@
+<h1>Product template page</h1>
